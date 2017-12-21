f6afc2ca1a6b32887c223f9a436c6afb1ca351b9
Nathan Misner <shicky256@gmail.com>
2017-12-21 10:26:57 -0500

    added player movement, animation

--- main.c
+++ main.c
@@ -3,17 +3,15 @@
 #include "library.h"		// NGPC routines
 #include "tiles.c" //graphics
 #include "map.h" //maps
+#include "main.h" //structs
 #include <stdlib.h>		// std C routines
 #include <stdio.h>
 
-u8 rstat,xPos,yPos,i,j,mapNum;
-
+u8 i,j,mapNum;
+SPRITE player;
 void drawMap(u8 mapNum);
 
 void main(void) {
-	xPos=0;
-	yPos=0;
-	rstat = USR_BOOT;
 	InitNGPC();
 	SysSetSystemFont();
 	
@@ -22,32 +20,59 @@ void main(void) {
 	ClearScreen(SCR_2_PLANE);
 	SetBackgroundColour(RGB(15, 15, 15));
 	SetPalette(SCR_2_PLANE, 0, 0, RGB(0,0,12), RGB(0,0,0), RGB(0,0,0));
-	//PutTile(SCR_2_PLANE,0,5,5,leftWall);
-	//loadMap(maps);
-	//drawMap(12);
+	player.spriteID=0;
+	player.tileNum=playerStandingL;
+	player.xPos=10;
+	player.yPos=40;
+	player.palette=0;
+	player.direction=0;
 	SetPalette(SPRITE_PLANE, 0, 0, RGB(0,0,0), RGB(12,12,2), RGB(15,15,0));
-	SetSprite(0,moon,0,xPos,yPos,0);
-	SetSprite(1,moon+1,1,8,0,0);
-	SetSprite(2,moon+2,1,-8,8,0);
-	SetSprite(3,moon+3,1,8,0,0);
+	SetSprite(player.spriteID,player.tileNum,0,player.xPos,player.yPos,player.palette);
+	SetSprite(player.spriteID+1,player.tileNum+1,1,0,8,0); //second sprite is linked to the first one
 	
 	mapNum=0;
 
 	while (1) {
-		if (JOYPAD & J_LEFT)
-			xPos--;
-		if (JOYPAD & J_RIGHT)
-			xPos++;
-		if (JOYPAD & J_UP)
-			yPos--;
-		if (JOYPAD & J_DOWN)
-			yPos++;
+		if (!JOYPAD)
+			player.direction=2;
+		if (JOYPAD & J_LEFT) {
+			player.xPos--;
+			player.direction=0;
+		}
+		if (JOYPAD & J_RIGHT) {
+			player.xPos++;
+			player.direction=1;
+		}
+		if (JOYPAD & J_UP) {
+			player.yPos--;
+			player.direction=1;
+		}
+		if (JOYPAD & J_DOWN) { 
+			player.yPos++;
+			player.direction=0;
+		}
 		if (JOYPAD & J_A)
 			mapNum++;
 		if (mapNum > 12)
 			mapNum=0;
 		drawMap(mapNum);
-		SetSprite(0,moon,0,xPos,yPos,0);
+		
+		switch (player.direction) {
+			case 0:
+			if (player.tileNum < playerStandingR || player.tileNum >= playerWalkingR2)
+				player.tileNum=playerStandingR;
+			else 
+				player.tileNum+=2;	
+			break;
+			case 1:
+			if (player.tileNum < playerStandingL || player.tileNum >= playerWalkingL2)
+				player.tileNum=playerStandingL;
+			else 
+				player.tileNum+=2;
+			break;
+		}
+		SetSprite(player.spriteID,player.tileNum,0,player.xPos,player.yPos,player.palette);
+		SetSprite(player.spriteID+1,player.tileNum+1,1,0,8,0);
 		WaitVsync();
 		
 	}
--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 typedef struct Sprite {
 	u8 spriteID;
+	u8 tileNum;
 	u8 xPos;
 	u8 yPos;
 	u8 palette;
-}
+	u8 direction;
+} SPRITE;
